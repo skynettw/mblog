989091937558b72481675c48bb4038dca940292d
Richard <skynet.tw@gmail.com>
2022-11-23 22:44:59 +0800

    add markdown

--- mysite/templatetags/markdown_extras.py
+++ mysite/templatetags/markdown_extras.py
@@ -1,12 +1,10 @@
+import markdown 
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
-import markdown as md
-
 register = template.Library()
 
-
-@register.filter()
-@stringfilter
-def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+@register.filter
+def convert_markdown(text):
+    return markdown.markdown(text)
