507b23f2463e6dc03670215d4cade718b2071085
Richard <skynet.tw@gmail.com>
2022-11-26 11:26:57 +0800

    for heroku upload

--- mblog/settings.py
+++ mblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!b_u+=p87g__246daxk)4s57e+1nkivltnxdil-2h!3@f8&b0!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
