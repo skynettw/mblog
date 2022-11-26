81916cfb950c0c112f1d88603ecfac08846681af
Richard <skynet.tw@gmail.com>
2022-11-26 11:22:19 +0800

    for heroku upload

--- mblog/settings.py
+++ mblog/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
