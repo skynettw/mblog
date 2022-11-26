ab9df3d3955043d75b1aa71580699b119c6e5217
Richard <skynet.tw@gmail.com>
2022-11-26 11:45:38 +0800

    for heroku upload

--- mblog/settings.py
+++ mblog/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 STATIC_URL = '/static/'
 
 
